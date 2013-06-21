a4b457de99c06cedcc163c274ad1f00b363e98d2
Piotrek Baranowski <bolczyslaw@gmail.com>
2013-06-21 22:28:44 +0200

    translated from coffescript to javascript

--- assets/javascripts/humanLibraryControllers.js
+++ assets/javascripts/humanLibraryControllers.js
@@ -1,93 +1,102 @@
 angular.module('humanLibrary.controllers', []).
   controller('LibraryCtrl', ['$scope', '$timeout', function($scope, $timeout) {
-//    bootstrap = ->
-//      $scope.library = {} unless $scope.library?
-//      $scope.library.books = [] unless $scope.library.books?
-//      $scope.library.nextId = 0 unless $scope.library.nextId?
-//      $scope.library.filters = {} unless $scope.library.filters?
-//      $scope.library.filters.showAvailable = true unless $scope.library.filters.showAvailable?
-//      $scope.library.filters.showRented = true unless $scope.library.filters.showRented?
-//      $scope.library.filters.showOnBreak = true unless $scope.library.filters.showOnBreak?
-//      $scope.library.filters.showAbsent = true unless $scope.library.filters.showAbsent?
-//
-//    #########################
-//    # tablica z książkami w bibliotece
-//    if Storage? and localStorage.library
-//      $scope.library = angular.fromJson(localStorage.library)
-//    bootstrap()
-//    $scope.overlay =
-//      visible: false
-//      data: ''
-//
-//    #########################
-//    # zapis do local storage
-//    $scope.save = ->
-//      localStorage.library = angular.toJson($scope.library) if Storage?
-//
-//    #########################
-//    # import
-//    $scope.import = ->
-//      $scope.library = angular.fromJson($scope.overlay.data)
-//      bootstrap()
-//      $scope.overlay.visible = false
-//      $scope.save()
-//
-//    #########################
-//    # usuń książkę o podanym indeksie z tablicy
-//    $scope.removeBook = (id) ->
-//      $scope.library.books.splice($scope.indexOfBookWithId(id), 1)
-//      $scope.save()
-//
-//    #########################
-//    # dodaj nową książkę na początku tablicy
-//    $scope.addBook = () ->
-//      $scope.library.books.push
-//        id: $scope.library.nextId
-//        nextId: 0
-//        name: ''
-//        status: 'available'
-//        niceStatus: ''
-//        period: 30
-//        rentals: []
-//        style: ''
-//        bar:
-//          progress: 100,
-//          color: '#00ff00',
-//      $scope.library.nextId += 1
-//      $scope.save()
-//
-//    #########################
-//    # przełącz status
-//    $scope.toggleStatus = (id, status) ->
-//      bookIndex = $scope.indexOfBookWithId(id)
-//      if $scope.library.books[bookIndex].status == status
-//        $scope.library.books[bookIndex].status = 'available'
-//      else
-//        $scope.library.books[bookIndex].status = status
-//      $scope.save()
-//
-//    #########################
-//    # usuń onformację o wypożyczeniu o podanym indeksie (książki i wypożyczenia)
-//    $scope.removeRental = (bookId, rentalId) ->
-//      bookIndex = $scope.indexOfBookWithId(bookId)
-//      rentalIndex = $scope.indexOfRentalWithId(bookIndex, rentalId)
-//      if rentalIndex == 0 and $scope.library.books[bookIndex].status == 'rented'
-//        $scope.returnBook bookId
-//      $scope.library.books[bookIndex].rentals.splice rentalIndex, 1
-//      $scope.save()
-//
-//
-//    #########################
-//    # ID NA INDEKSY
-//    # funkcja pomocnicza
-//    indexById = (array, id) ->
-//      for entry, i in array
-//        return i if entry.id == id
-//    # książki
-//    $scope.indexOfBookWithId = (id) ->
-//      indexById($scope.library.books, id)
-//    # wypożyczenia
-//    $scope.indexOfRentalWithId = (index, id) ->
-//      indexById($scope.library.books[index].rentals, id)    
+    var bootstrap = function() {
+      $scope.library = $scope.library || {};
+      $scope.library.books = $scope.library.books || [];
+      $scope.library.nextId = $scope.library.nextId || 0;
+      $scope.library.filters = $scope.library.filters || {};
+      $scope.library.filters.showAvailable = $scope.library.filters.showAvailable || true;
+      $scope.library.filters.showRented = $scope.library.filters.showRented || true;
+      $scope.library.filters.showOnBreak = $scope.library.filters.showOnBreak || true;
+      $scope.library.filters.showAbsent = $scope.library.filters.showAbsent || true;
+    };
+
+    if ((typeof Storage !== "undefined" && Storage !== null) && localStorage.library) {
+      $scope.library = angular.fromJson(localStorage.library);
+    }
+
+    bootstrap();
+
+    $scope.overlay = {
+      visible: false,
+      data: ''
+    };
+
+    $scope.save = function() {
+      if (typeof Storage !== "undefined" && Storage !== null) {
+        return localStorage.library = angular.toJson($scope.library);
+      }
+    };
+
+    $scope["import"] = function() {
+      $scope.library = angular.fromJson($scope.overlay.data);
+      bootstrap();
+      $scope.overlay.visible = false;
+      return $scope.save();
+    };
+
+    $scope.removeBook = function(id) {
+      $scope.library.books.splice($scope.indexOfBookWithId(id), 1);
+      return $scope.save();
+    };
+
+    $scope.addBook = function() {
+      $scope.library.books.push({
+        id: $scope.library.nextId,
+        nextId: 0,
+        name: '',
+        status: 'available',
+        niceStatus: '',
+        period: 30,
+        rentals: [],
+        style: '',
+        bar: {
+          progress: 100,
+          color: '#00ff00'
+        }
+      });
+      $scope.library.nextId += 1;
+      return $scope.save();
+    };
+
+    $scope.toggleStatus = function(id, status) {
+      var bookIndex;
+      bookIndex = $scope.indexOfBookWithId(id);
+      if ($scope.library.books[bookIndex].status === status) {
+        $scope.library.books[bookIndex].status = 'available';
+      } else {
+        $scope.library.books[bookIndex].status = status;
+      }
+      return $scope.save();
+    };
+
+    $scope.removeRental = function(bookId, rentalId) {
+      var bookIndex, rentalIndex;
+      bookIndex = $scope.indexOfBookWithId(bookId);
+      rentalIndex = $scope.indexOfRentalWithId(bookIndex, rentalId);
+      if (rentalIndex === 0 && $scope.library.books[bookIndex].status === 'rented') {
+        $scope.returnBook(bookId);
+      }
+      $scope.library.books[bookIndex].rentals.splice(rentalIndex, 1);
+      return $scope.save();
+    };
+
+    var indexById = function(array, id) {
+      var entry, i, _i, _len;
+      for (i = _i = 0, _len = array.length; _i < _len; i = ++_i) {
+        entry = array[i];
+        if (entry.id === id) {
+          return i;
+        }
+      }
+    };
+
+    $scope.indexOfBookWithId = function(id) {
+      return indexById($scope.library.books, id);
+    };
+
+    $scope.indexOfRentalWithId = function(index, id) {
+      return indexById($scope.library.books[index].rentals, id);
+    };
   }]);
 
--- assets/javascripts/humanLibraryDirectives.js
+++ assets/javascripts/humanLibraryDirectives.js
@@ -1,108 +1,106 @@
 angular.module('humanLibrary.directives', []).
   directive('library', ['$timeout', function ($timeout) {
     return function(scope, element, attrs) {
-      
+
+      var everySecond, everySecondId, f, updateRentedStatuses;
+
+      updateRentedStatuses = function() {
+        var book, diffrence, i, minutes, minutess, over, seconds, secondss, _i, _len, _ref, _results;
+        _ref = scope.library.books;
+        _results = [];
+        for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
+          book = _ref[i];
+          if (book.status === 'rented') {
+            diffrence = book.rentals[0].period * 60000 - ((new Date()).getTime() - book.rentals[0].rented);
+            if (diffrence < 0) {
+              over = true;
+              diffrence *= -1;
+            } else {
+              over = false;
+            }
+            minutess = minutes = Math.floor(diffrence / 60000);
+            if (minutes < 10) {
+              minutess = "0" + minutes;
+            }
+            secondss = seconds = Math.floor(diffrence % 60000 / 1000);
+            if (seconds < 10) {
+              secondss = "0" + seconds;
+            }
+            if (over) {
+              book.niceStatus = "-" + minutess + ":" + secondss;
+              book.bar.progress = 100;
+              _results.push(book.bar.color = '#d3401e');
+            } else {
+              book.niceStatus = "" + minutess + ":" + secondss;
+              book.bar.progress = 100 - diffrence / (book.rentals[0].period * 600);
+              if (minutes < 5) {
+                _results.push(book.bar.color = '#d3401e');
+              } else {
+                _results.push(book.bar.color = '#fff200');
+              }
+            }
+          } else {
+            _results.push(void 0);
+          }
+        }
+        return _results;
+      };
+
+      updateRentedStatuses();
+
+      everySecondId = void 0;
+
+      everySecond = function() {
+        var onTimeout;
+        onTimeout = function() {
+          updateRentedStatuses();
+          return everySecond();
+        };
+        return everySecondId = $timeout(onTimeout, 1000);
+      };
+
+      everySecond();
+
+      scope.rentBook = function(id) {
+        var bookIndex;
+        bookIndex = scope.indexOfBookWithId(id);
+        if (scope.library.books[bookIndex].status === 'available') {
+          scope.library.books[bookIndex].rentals.unshift({
+            id: scope.library.books[bookIndex].nextId,
+            rented: (new Date()).getTime(),
+            period: scope.library.books[bookIndex].period
+          });
+          scope.library.books[bookIndex].nextId += 1;
+          scope.library.books[bookIndex].status = 'rented';
+          updateRentedStatuses();
+          return scope.save();
+        }
+      };
+
+      scope.returnBook = function(id) {
+        var bookIndex;
+        bookIndex = scope.indexOfBookWithId(id);
+        if (scope.library.books[bookIndex].status === 'rented') {
+          scope.library.books[bookIndex].rentals[0].returned = (new Date()).getTime();
+          scope.library.books[bookIndex].status = 'available';
+          scope.library.books[bookIndex].niceStatus = '';
+          scope.library.books[bookIndex].bar.progress = 100;
+          scope.library.books[bookIndex].bar.color = '#00ff00';
+          scope.library.books[bookIndex].period = 30;
+          return scope.save();
+        }
+      };
+
+      f = function(newValue, oldValue) {
+        return scope.save();
+      };
+
+      scope.$watch('library.filters', f, true);
+
+      scope.$watch('overlay.visible', function(newValue, oldValue) {
+        scope.save();
+        return scope.overlay.data = angular.toJson(scope.library);
+      });
+
     };
   }]);
-    
-//    ###########################
-//    # uaktualnia wszystkie statusy wypożyczone
-//    updateRentedStatuses = ->
-//      for book, i in scope.library.books
-//        if book.status == 'rented'
-//          diffrence = book.rentals[0].period * 60000 - ((new Date()).getTime() - book.rentals[0].rented)
-//          if diffrence < 0
-//            over = true
-//            diffrence *= -1
-//          else
-//            over = false
-//          minutess = minutes = Math.floor(diffrence/60000)
-//          minutess = "0#{minutes}" if minutes < 10
-//          secondss = seconds = Math.floor(diffrence%60000/1000)
-//          secondss = "0#{seconds}" if seconds < 10
-//          if over
-//            book.niceStatus = "-#{minutess}:#{secondss}"
-//            book.bar.progress = 100
-//            book.bar.color = '#d3401e'
-//          else
-//            book.niceStatus = "#{minutess}:#{secondss}"
-//            book.bar.progress = 100 - diffrence / (book.rentals[0].period * 600)
-//            if minutes < 5
-//              book.bar.color = '#d3401e'
-//            else
-//              book.bar.color = '#fff200'
-//    updateRentedStatuses()
-//    
-//    everySecondId = undefined
-//    everySecond = ->
-//      onTimeout = ->
-//        updateRentedStatuses()
-//        everySecond()
-//      everySecondId = $timeout(onTimeout, 1000)
-//    everySecond()
-//      
-//
-//    #########################
-//    # wypożycz książkę o podanym indeksie
-//    scope.rentBook = (id) ->
-//      bookIndex = scope.indexOfBookWithId(id)
-//      if scope.library.books[bookIndex].status == 'available'
-//        scope.library.books[bookIndex].rentals.unshift
-//          id: scope.library.books[bookIndex].nextId
-//          rented: (new Date()).getTime(),
-//          period: scope.library.books[bookIndex].period
-//        scope.library.books[bookIndex].nextId += 1
-//        scope.library.books[bookIndex].status = 'rented'
-//        updateRentedStatuses()
-//        scope.save()
-//  
-//    #########################
-//    # zwróć książkę o podanym indeksie
-//    scope.returnBook = (id) ->
-//      bookIndex = scope.indexOfBookWithId(id)
-//      if scope.library.books[bookIndex].status == 'rented'
-//        scope.library.books[bookIndex].rentals[0].returned = (new Date()).getTime()
-//        scope.library.books[bookIndex].status = 'available'
-//        scope.library.books[bookIndex].niceStatus = ''
-//        scope.library.books[bookIndex].bar.progress = 100
-//        scope.library.books[bookIndex].bar.color = '#00ff00'
-//        scope.library.books[bookIndex].period = 30
-//        scope.save()
-//
-//    f = (newValue, oldValue) ->
-//      scope.save()
-//    scope.$watch 'library.filters', f, true
-//
-//    scope.$watch 'overlay.visible', (newValue, oldValue) ->
-//      scope.save()
-//      scope.overlay.data = angular.toJson(scope.library)
-//
-//libraryDirective.$inject = ['$timeout']
-//
-//libraryModule.directive 'book', ->
-//  (scope, element, attrs) ->
-//    scope.toggleMore = ->
-//      if scope.book.style == ''
-//        scope.book.style = 'height: 300px; margin-top: 10px; margin-bottom: 10px;'
-//      else
-//        scope.book.style = ''
-//      scope.save()
-//    
-//    scope.$watch 'book.notes', ->
-//      scope.save()
-//    scope.$watch 'book.name', ->
-//      scope.save()
-//
-//    scope.$watch 'book.break', (newValue, oldValue) ->
-//      if newValue
-//        scope.book.bar.color = '#e8e8e8'
-//      else
-//        scope.book.bar.color = '#00ff00'
-//      scope.save()
-//
-//    scope.$watch 'book.absent', (newValue, oldValue) ->
-//      if newValue
-//        scope.book.bar.color = '#e8e8e8'
-//      else
-//        scope.book.bar.color = '#00ff00'
-//      scope.save()
