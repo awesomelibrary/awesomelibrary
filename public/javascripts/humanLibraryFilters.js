9e9f62ee2efb6f854c4d7c4f8d036743f2b915f9
Piotrek Baranowski <bolczyslaw@gmail.com>
2013-09-28 23:11:01 +0200

    Books names and library view

--- public/javascripts/humanLibraryFilters.js
+++ public/javascripts/humanLibraryFilters.js
@@ -1,12 +1,26 @@
 angular.module('humanLibrary.filters', []).
-  filter('status', function() {
-    return function(input, filters) {
-      var output = [];
-      for (var i = 0; i < input.length; i++) {
-        if ((filters.showAvailable && input[i].status == 'available') || (filters.showRented && input[i].status == 'rented') || (filters.showOnBreak && input[i].status == 'break') || (filters.showAbsent && input[i].status == 'absent')) {
-          output.push(input[i]);
-        }
-      }
-      return output;
-    };
-  });
+        filter('status', function() {
+            return function(input, filters) {
+                var output = [];
+                for (var i = 0; i < input.length; i++) {
+                    if ((filters.showAvailable && input[i].status == 'available') || (filters.showRented && input[i].status == 'rented') || (filters.showOnBreak && input[i].status == 'break') || (filters.showAbsent && input[i].status == 'absent')) {
+                        output.push(input[i]);
+                    }
+                }
+                return output;
+            };
+        }).
+        filter('every', function() {
+            return function(input, number) {
+                // if it is not array, return null
+                if (!angular.isArray) { return input; }
+                
+                var i = 0, output = [];
+                while (i < input.length) {
+                    output.push(input.slice(i, i+number));
+                    i += number;
+                }
+                
+                return output;
+            };
+        });
--- public/javascripts/humanLibraryServices.js
+++ public/javascripts/humanLibraryServices.js
@@ -1,37 +1,48 @@
-angular.module('humanLibrary.services', []).
-  factory('$book', [function() {
+angular.module('humanLibrary.services', ['humanLibrary.filters']).
+        factory('$book', [function() {
 
-    // Book constructor
-    var Book = function() {
-    };
-      
-    return Book;
-    
-  }]).
-  factory('$library', ['$book', function($book) {
+                // Book constructor
+                var Book = function() {
+                    this.name = "";
+                };
 
-    // Library constructor
-    var Library = function() {
-      this.books = [];
-    };
+                return Book;
 
-    // Adding book to the library
-    Library.prototype.addBook = function() {
-      this.books.push(new $book());
-    };
+            }]).
+        factory('$library', ['$filter', '$book', function($filter, $book) {
 
-    // Deleting book from the library
-    Library.prototype.deleteBook = function(book) {
-      if (angular.isArray(this.books)) {
-        var index = this.books.indexOf(book);
-        if (index !== -1) { 
-          this.books.splice(index, 1);
-        }
-        return index;
-      }
-      return -1;
-    };
+                var every = $filter('every');
 
-    return Library;
-    
-  }]);
+                // Library constructor
+                var Library = function() {
+                    this.books = [];
+                    this.groupedBooks = [];
+                    this.booksInGroup = 3;
+                };
+
+                // Adding book to the library
+                Library.prototype.addBook = function() {
+                    this.books.push(new $book());
+                    this.regroupBooks();
+                };
+
+                // Deleting book from the library
+                Library.prototype.deleteBook = function(book) {
+                    if (angular.isArray(this.books)) {
+                        var index = this.books.indexOf(book);
+                        if (index !== -1) {
+                            this.books.splice(index, 1);
+                            this.regroupBooks();
+                        }
+                        return index;
+                    }
+                    return -1;
+                };
+
+                Library.prototype.regroupBooks = function() {
+                    this.groupedBooks = every(this.books, this.booksInGroup);
+                };
+
+                return Library;
+
+            }]);
